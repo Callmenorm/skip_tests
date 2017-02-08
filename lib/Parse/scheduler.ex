e1cdae129d7bf9c4fd5e4bc8072118ad9b2d2f48
Michael Anderson <normanganderson@gmail.com>
2017-02-07 21:57:44 -0700

    Added comments and assumptions I made

--- lib/Parse/scheduler.ex
+++ lib/Parse/scheduler.ex
@@ -15,31 +15,36 @@ defmodule SkipTests.Parse.Scheduler do
     def check_and_parse_files() do
         files = SkipTests.Parse.XML.look_for_files(@xml_dir)
         |> Enum.map(fn(filename) -> {filename, File.read(filename)} end)
-        |> Enum.filter(fn(x) ->
+        |> Enum.filter(fn(x) -> #filters out the unreadable files
             case x do
                 {_filename, {:ok, _xml_str}} ->
                     true
                 _ -> false
             end
         end)
-        |> Enum.map(fn(x) ->
+        |> Enum.map(fn(x) -> #returns a tuple of filename and map of retail items
             {filename, {:ok, xml_str}} = x
+
+            # error handling is probably going to blow this up
+            # I'm not sure what happens when Quinn fails. Documentation is scarce
+            # but it was very easy to use
             retail_items = SkipTests.Parse.XML.get_retail_items(xml_str)
-            [head | _tail] = retail_items
+            [head | _tail] = retail_items #assumes that there is only one retail items element
             %{value: values} = head
+            #maps the ItemNo to the retail item element
             mapped_values = Enum.reduce(values, %{}, fn(value, acc) ->
                 mapped_value = Enum.into(value.attr, %{})
                 Map.put_new(acc, mapped_value[:ItemNo], mapped_value)
             end)
             {filename, mapped_values}
         end)
-        |> Enum.map(fn(x) ->
-                {filename, _} = x
-                "#{@xml_dir}/" <> file = filename
+        |> Enum.map(fn(x) -> #moves the files to the parsed directory
+            {filename, _} = x
+            "#{@xml_dir}/" <> file = filename
 
-                File.rename(filename, "#{@parsed_dir}/#{file}")
-                x
-            end)
+            File.rename(filename, "#{@parsed_dir}/#{file}")
+            x
+        end)
         {:ok, files}
     end
 
@@ -47,6 +52,7 @@ defmodule SkipTests.Parse.Scheduler do
         set_timeout()
         case check_and_parse_files() do
             {:ok, parsed_files} ->
+                # maps the filename to the mapped retails items
                 new_state = Enum.reduce(parsed_files, state, fn(file, acc_state) ->
                         {filename, mapped_values} = file
                         Map.put_new(acc_state, filename, mapped_values)
@@ -58,6 +64,6 @@ defmodule SkipTests.Parse.Scheduler do
     end
 
     defp set_timeout() do
-        Process.send_after(self(), :check, 1000 * 60 * 1) # send :check after 5 minutes
+        Process.send_after(self(), :check, 1000 * 60 * 5) # send :check after 5 minutes
     end
 end
